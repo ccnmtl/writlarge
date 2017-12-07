467e2ae47c691bffbabc6f85bd0206cb80be04d0
Susan Dreher <sdreher@columbia.edu>
2017-12-07 15:49:56 -0500

    Boilerplate functionality * authorization * reset password * change password * clean bootstrap4 template

--- writlarge/main/tests/test_views.py
+++ writlarge/main/tests/test_views.py
@@ -1,5 +1,8 @@
+from json import loads
 from django.test import TestCase
 from django.test.client import Client
+
+from writlarge.main.tests.factories import UserFactory
 
 
 class BasicTest(TestCase):
@@ -14,3 +17,78 @@ class BasicTest(TestCase):
         response = self.c.get("/smoketest/")
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, 'PASS')
+
+
+class PasswordTest(TestCase):
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.user = UserFactory()
+
+    def test_logged_out(self):
+        response = self.client.get('/accounts/password_change/')
+        self.assertEquals(response.status_code, 302)
+
+        response = self.client.get('/accounts/password_reset/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_logged_in(self):
+        self.assertTrue(self.client.login(
+            username=self.user.username, password="test"))
+        response = self.client.get('/accounts/password_change/')
+        self.assertEquals(response.status_code, 200)
+
+        response = self.client.get('/accounts/password_reset/')
+        self.assertEquals(response.status_code, 200)
+
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+
+        self.user = UserFactory()
+
+    def test_login_get(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEquals(response.status_code, 405)
+
+    def test_login_post_noajax(self):
+        response = self.client.post('/accounts/login/',
+                                    {'username': self.user.username,
+                                     'password': 'test'})
+        self.assertEquals(response.status_code, 405)
+
+    def test_login_post_ajax(self):
+        response = self.client.post('/accounts/login/',
+                                    {'username': '',
+                                     'password': ''},
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.status_code, 200)
+        the_json = loads(response.content)
+        self.assertTrue(the_json['error'], True)
+
+        response = self.client.post('/accounts/login/',
+                                    {'username': self.user.username,
+                                     'password': 'test'},
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.status_code, 200)
+        the_json = loads(response.content)
+        self.assertTrue(the_json['next'], "/")
+        self.assertTrue('error' not in the_json)
+
+
+class LogoutTest(TestCase):
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+
+        self.user = UserFactory()
+
+    def test_logout_user(self):
+        self.client.login(username=self.user.username, password="test")
+
+        response = self.client.get('/accounts/logout/?next=/', follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('Log In' in response.content)
+        self.assertFalse('Log Out' in response.content)
--- writlarge/urls.py
+++ writlarge/urls.py
@@ -1,32 +1,52 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import logout
-from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import (
+    password_change,
+    password_change_done, password_reset, password_reset_done,
+    password_reset_confirm,
+    password_reset_complete)
 from django.views.generic import TemplateView
 from django.views.static import serve
+
 from writlarge.main import views
-import os.path
 
-site_media_root = os.path.join(os.path.dirname(__file__), "../media")
 
-redirect_after_logout = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
+admin.autodiscover()
+
+
 auth_urls = url(r'^accounts/', include('django.contrib.auth.urls'))
-logout_page = url(
-    r'^accounts/logout/$',
-    logout,
-    {'next_page': redirect_after_logout})
 if hasattr(settings, 'CAS_BASE'):
-    from djangowind.views import logout as windlogout
     auth_urls = url(r'^accounts/', include('djangowind.urls'))
-    logout_page = url(
-        r'^accounts/logout/$',
-        windlogout,
-        {'next_page': redirect_after_logout})
+
 
 urlpatterns = [
-    auth_urls,
-    logout_page,
     url(r'^$', views.IndexView.as_view()),
+
+    url(r'^accounts/login/$', views.LoginView.as_view()),
+    url(r'^accounts/logout/$', views.LogoutView.as_view()),
+
+    # password change & reset. overriding to gate them.
+    url(r'^accounts/password_change/$',
+        login_required(password_change),
+        name='password_change'),
+    url(r'^accounts/password_change/done/$',
+        login_required(password_change_done),
+        name='password_change_done'),
+    url(r'^password/reset/$',
+        password_reset,
+        name='password_reset'),
+    url(r'^password/reset/done/$', password_reset_done,
+        name='password_reset_done'),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password/reset/complete/$',
+        password_reset_complete, name='password_reset_complete'),
+
+    auth_urls,
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^_impersonate/', include('impersonate.urls')),
     url(r'^stats/$', TemplateView.as_view(template_name="stats.html")),
