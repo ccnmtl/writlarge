df76f33db5aeb40bec79fdb1bbb205e5e911ab90
Susan Dreher <sdreher@columbia.edu>
2018-02-01 14:41:20 -0500

    Simplify login flow
    Use the standard Django flow rather than a modal

--- writlarge/main/tests/test_views.py
+++ writlarge/main/tests/test_views.py
@@ -1,4 +1,3 @@
-from json import loads
 from django.test import TestCase
 from django.test.client import Client
 
@@ -40,55 +39,3 @@ class PasswordTest(TestCase):
 
         response = self.client.get('/accounts/password_reset/')
         self.assertEquals(response.status_code, 200)
-
-
-class LoginTest(TestCase):
-
-    def setUp(self):
-        super(TestCase, self).setUp()
-
-        self.user = UserFactory()
-
-    def test_login_get(self):
-        response = self.client.get('/accounts/login/')
-        self.assertEquals(response.status_code, 405)
-
-    def test_login_post_noajax(self):
-        response = self.client.post('/accounts/login/',
-                                    {'username': self.user.username,
-                                     'password': 'test'})
-        self.assertEquals(response.status_code, 405)
-
-    def test_login_post_ajax(self):
-        response = self.client.post('/accounts/login/',
-                                    {'username': '',
-                                     'password': ''},
-                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        self.assertEquals(response.status_code, 200)
-        the_json = loads(response.content)
-        self.assertTrue(the_json['error'], True)
-
-        response = self.client.post('/accounts/login/',
-                                    {'username': self.user.username,
-                                     'password': 'test'},
-                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        self.assertEquals(response.status_code, 200)
-        the_json = loads(response.content)
-        self.assertTrue(the_json['next'], "/")
-        self.assertTrue('error' not in the_json)
-
-
-class LogoutTest(TestCase):
-
-    def setUp(self):
-        super(TestCase, self).setUp()
-
-        self.user = UserFactory()
-
-    def test_logout_user(self):
-        self.client.login(username=self.user.username, password="test")
-
-        response = self.client.get('/accounts/logout/?next=/', follow=True)
-        self.assertEquals(response.status_code, 200)
-        self.assertTrue('Log In' in response.content)
-        self.assertFalse('Log Out' in response.content)
--- writlarge/main/views.py
+++ writlarge/main/views.py
@@ -1,42 +1,15 @@
 from django.conf import settings
-from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import logout as auth_logout_view
-from django.views.generic.base import TemplateView, View
-from djangowind.views import logout as wind_logout_view
-
-from writlarge.main.mixins import JSONResponseMixin
+from django.views.generic.base import TemplateView
 
 
 # returns important setting information for all web pages.
+# returns important setting information for all web pages.
 def django_settings(request):
-    return {'settings':
-            {'GOOGLE_MAP_API': getattr(settings, 'GOOGLE_MAP_API', '')}}
+    whitelist = ['GOOGLE_ANALYTICS_ID', 'CAS_BASE']
+
+    return {'settings': dict([(k, getattr(settings, k, None))
+                              for k in whitelist])}
 
 
 class IndexView(TemplateView):
     template_name = "main/index.html"
-
-
-class LoginView(JSONResponseMixin, View):
-
-    def post(self, request):
-        request.session.set_test_cookie()
-        login_form = AuthenticationForm(request, request.POST)
-        if login_form.is_valid():
-            login(request, login_form.get_user())
-            if request.user is not None:
-                next_url = request.POST.get('next', '/')
-                return self.render_to_json_response({'next': next_url})
-
-        return self.render_to_json_response({'error': True})
-
-
-class LogoutView(LoginRequiredMixin, View):
-
-    def get(self, request):
-        if hasattr(settings, 'CAS_BASE'):
-            return wind_logout_view(request, next_page="/")
-        else:
-            return auth_logout_view(request, "/")
