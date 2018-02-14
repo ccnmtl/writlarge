074d0b3f770c2bfe141e9f8aec70e9083a6b593d
Susan Dreher <sdreher@columbia.edu>
2018-02-14 18:32:47 -0500

    Drop a new pin
    Show an add dialog

--- media/js/src/components/gmapvue.js
+++ media/js/src/components/gmapvue.js
@@ -9,8 +9,27 @@ var GoogleMapVue = {
             sites: [],
             map: null,
             markers: [],
-            bounds: null
+            bounds: null,
+            newPin: null
         };
+    },
+    methods: {
+        dropPin: function(event) {
+            if (this.newPin) {
+                this.newPin.setMap(null);
+            }
+
+            this.newPin = new google.maps.Marker({
+                position: event.latLng,
+                map: this.map
+            });
+
+            // reverse geocode?
+        },
+        savePin: function(event) {
+        },
+        editPin: function(event) {
+        }
     },
     created: function() {
         const url = WritLarge.baseUrl + 'api/site/';
@@ -22,17 +41,24 @@ var GoogleMapVue = {
         this.bounds = new google.maps.LatLngBounds();
         const elt = document.getElementById(this.mapName);
         this.map = new google.maps.Map(elt);
+
+        this.map.addListener('click', (ev) => {
+            this.dropPin(ev);
+        });
     },
     updated: function() {
         this.sites.forEach((site) => {
             const position = new google.maps.LatLng(
                 site.latitude, site.longitude);
             const marker = new google.maps.Marker({
-                position,
+                position: position,
                 map: this.map
             });
             this.markers.push(marker);
-            this.map.fitBounds(this.bounds.extend(position));
+            if (!this.newPin) {
+                // don't change the viewport
+                this.map.fitBounds(this.bounds.extend(position));
+            }
         });
     }
 };
