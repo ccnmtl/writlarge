37e6796834ff51dab21a9669b2bf0482eb2a6efe
Susan Dreher <sdreher@gmail.com>
2018-05-04 12:41:33 -0400

    eslint'ing

--- media/js/src/components/familynetworkvue.js
+++ media/js/src/components/familynetworkvue.js
@@ -169,7 +169,8 @@ var FamilyNetworkVue = {
                 .style('fill', function(node) {
                     return 'url(#' + node.group + ')';
                 })
-                .on("click", function (d) {
+                .on('click', function(d) {
+                    // eslint-disable-next-link scanjs-rules/assign_to_href
                     location.href = '/view/' + d.id + '/';
                 });
 
