03979d57087100dc5a17aafea1b0610e5ade23bd
Susan Dreher <sdreher@gmail.com>
2018-08-02 11:52:45 -0400

    remove urls signature in preparation for djangowind upgrade

--- writlarge/urls.py
+++ writlarge/urls.py
@@ -1,12 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import (
-    password_change,
-    password_change_done, password_reset, password_reset_done,
-    password_reset_confirm,
-    password_reset_complete)
 from django.urls.conf import path
 from django.views.generic import TemplateView
 from django.views.static import serve
@@ -32,24 +26,6 @@ router.register(r'place', views.PlaceViewSet)
 urlpatterns = [
     url(r'^$', views.CoverView.as_view()),
     url(r'^api/', include(router.urls)),
-
-    # password change & reset. overriding to gate them.
-    url(r'^accounts/password_change/$',
-        login_required(password_change),
-        name='password_change'),
-    url(r'^accounts/password_change/done/$',
-        login_required(password_change_done),
-        name='password_change_done'),
-    url(r'^password/reset/$',
-        password_reset,
-        name='password_reset'),
-    url(r'^password/reset/done/$', password_reset_done,
-        name='password_reset_done'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        password_reset_confirm,
-        name='password_reset_confirm'),
-    url(r'^password/reset/complete/$',
-        password_reset_complete, name='password_reset_complete'),
 
     auth_urls,
 
